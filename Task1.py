c28ed9d69e55408b82a5e9e69a153e4fb3252fbf
Emma Saroyan <emma.saroyan@gmail.com>
2020-05-04 16:10:37 +0400

    Added file

---
+++ Task1.py
@@ -0,0 +1,36 @@
+"""
+Read file into texts and calls.
+It's ok if you don't understand how to read files.
+"""
+import csv
+with open('texts.csv', 'r') as f:
+    reader = csv.reader(f)
+    texts = list(reader)
+
+with open('calls.csv', 'r') as f:
+    reader = csv.reader(f)
+    calls = list(reader)
+
+
+"""
+TASK 1:
+How many different telephone numbers are there in the records?
+Print a message:
+"There are <count> different telephone numbers in the records."
+"""
+
+count = []
+
+for record in texts:
+    if record[0] not in count:
+        count.append(record[0])
+    if record[1] not in count:
+        count.append(record[1])
+
+for record in calls:
+    if record[0] not in count:
+        count.append(record[0])
+    if record[1] not in count:
+        count.append(record[1])
+
+print("There are " + str(len(count)) + " different telephone numbers in the records.")
